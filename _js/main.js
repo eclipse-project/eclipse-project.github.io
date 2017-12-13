5b53d1eaaca9ea37ab01b03543080e7f11efd98d
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2017-12-13 20:40:59 +0100

    Add animations in homepage

--- _js/main.js
+++ _js/main.js
@@ -1,3 +1,17 @@
 import $ from "jquery";
+import ScrollReveal from "scrollreveal";
 
 console.log($);
+$(document).ready(function() {
+  window.sr = ScrollReveal({
+    distance: '5rem',
+    scale: 1,
+    delay: 100,
+    afterReveal: function(element) {
+      $(element).addClass('revealed');
+    }
+  });
+  sr.reveal('.left-aligned .reveal', {origin: 'left'});
+  sr.reveal('.right-aligned .reveal', {origin: 'right'});
+  sr.reveal('.center-aligned .reveal', {origin: 'bottom'});
+});
