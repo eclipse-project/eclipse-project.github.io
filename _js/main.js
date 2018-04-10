1d32bab726fb317b728d8b6e54f22c1db26755ea
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2018-04-10 20:32:33 +0200

    Improve navigation menu

--- _js/main.js
+++ _js/main.js
@@ -9,7 +9,9 @@ $(document).ready(function() {
       let $target = $('#' + $this.data('target'));
       $this.toggleClass('is-active')
       $target.toggleClass('is-active');
+      $('.navbar').toggleClass('slideInDown');
       $this.closest('nav').toggleClass('is-active');
+      $('.languages').addClass('hidden fadeOutDown').removeClass('fadeInUp');
     });
 
   window.sr = ScrollReveal({
@@ -24,12 +26,32 @@ $(document).ready(function() {
   sr.reveal('.right-aligned .reveal', {origin: 'right'});
   sr.reveal('.center-aligned .reveal', {origin: 'bottom'});
 
-  var articleContent = $('.article').html();
-  var iframePart1 = '<div class="youtube-video-wrapper"><iframe width="560" height="315" src="https://www.youtube.com/embed/';
-  var iframePart2 = '?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
-  var re1 = new RegExp('<p>%youtube ', 'g');
-  var re2 = new RegExp('%</p>', 'g');
-  var articleContent = articleContent.replace(re1, iframePart1);
-  var articleContent = articleContent.replace(re2, iframePart2);
-  $('.article').html(articleContent);
+  renderYoutubeVideos();
+
+  $('.has-dropdown').on('click', function() {
+    $(this).toggleClass('is-active');
+  });
+
+  $('.language-selector .navbar-item').on('click', function() {
+    $(this).find('.mdi').toggleClass('mdi-flip-v');
+  });
+
+  $('.language-selector-mobile-trigger').on('click', function() {
+    $('.languages').removeClass('hidden').toggleClass('fadeInUp fadeOutDown');
+  });
 });
+
+function renderYoutubeVideos() {
+  var $article = $('.article');
+  if ($article.length) {
+    console.log($article);
+    var articleContent = $article.html();
+    var iframePart1 = '<div class="youtube-video-wrapper"><iframe width="560" height="315" src="https://www.youtube.com/embed/';
+    var iframePart2 = '?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
+    var re1 = new RegExp('<p>%youtube ', 'g');
+    var re2 = new RegExp('%</p>', 'g');
+    var articleContent = articleContent.replace(re1, iframePart1);
+    var articleContent = articleContent.replace(re2, iframePart2);
+    $article.html(articleContent);
+  }
+}
